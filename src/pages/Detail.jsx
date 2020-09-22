b4d209c0fff70827d7932f0ba2695d15b563a618
Amiruddin <amiruddinid@gmail.com>
2020-09-22 15:00:32 +0700

    fetch with params

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,10 +2,25 @@ import React, { Component } from 'react'
 import {withRouter} from 'react-router-dom';
 
 class Detail extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : {
+                title: '',
+                id : 0,
+            }
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/todos/' + this.props.match.params.id)
+            .then(response => response.json())
+            .then(json => this.setState({data:json}))
+    }
     render() {
         return (
             <div>
-                <h1>Detail {this.props.match.params.id}</h1>
+                <h1>{this.state.data.title}</h1>
+                <h1>{this.state.data.id}</h1>
             </div>
         )
     }
